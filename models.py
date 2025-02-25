b1d977a295dd74aa492b282da41143a2ee3e6e99
god <82193851+yuntaoshou@users.noreply.github.com>
2025-02-25 15:04:10 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,395 @@
+import os
+import os.path as osp
+import shutil
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch_geometric.data import InMemoryDataset, download_url, extract_zip
+from torch_geometric.io import read_tu_data
+from torch.nn import Linear
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import global_mean_pool
+from typing import Optional, Tuple, NamedTuple, List
+
+from itertools import repeat, product
+import numpy as np
+
+from copy import deepcopy
+import pdb
+
+import torch
+from abc import ABC, abstractmethod
+from torch_scatter import scatter
+from torch_geometric.nn import GINConv, global_add_pool
+
+from torch.distributions.bernoulli import Bernoulli
+from torch_geometric.utils import dropout_adj, to_networkx, to_undirected, degree, to_scipy_sparse_matrix, \
+    from_scipy_sparse_matrix, sort_edge_index, add_self_loops, subgraph
+
+from FourierGNN import FGN
+
+class Loss(ABC):
+    @abstractmethod
+    def compute(self, anchor, sample, pos_mask, neg_mask, *args, **kwargs) -> torch.FloatTensor:
+        pass
+
+    def __call__(self, anchor, sample, pos_mask=None, neg_mask=None, *args, **kwargs) -> torch.FloatTensor:
+        loss = self.compute(anchor, sample, pos_mask, neg_mask, *args, **kwargs)
+        return loss
+
+def _similarity(h1: torch.Tensor, h2: torch.Tensor):
+    h1 = F.normalize(h1)
+    h2 = F.normalize(h2)
+    return h1 @ h2.t()
+
+class InfoNCE(Loss):
+    def __init__(self, tau):
+        super(InfoNCE, self).__init__()
+        self.tau = tau
+
+    def compute(self, anchor, sample, pos_mask, neg_mask, *args, **kwargs):
+        sim = _similarity(anchor, sample) / self.tau
+        exp_sim = torch.exp(sim) * (pos_mask + neg_mask)
+        log_prob = sim - torch.log(exp_sim.sum(dim=1, keepdim=True))
+        loss = log_prob * pos_mask
+        loss = loss.sum(dim=1) / pos_mask.sum(dim=1)
+        return -loss.mean()
+
+class Graph(NamedTuple):
+    x: torch.FloatTensor
+    edge_index: torch.LongTensor
+    edge_weights: Optional[torch.FloatTensor]
+
+    def unfold(self) -> Tuple[torch.FloatTensor, torch.LongTensor, Optional[torch.FloatTensor]]:
+        return self.x, self.edge_index, self.edge_weights
+
+
+class Augmentor(ABC):
+    """Base class for graph augmentors."""
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def augment(self, g: Graph) -> Graph:
+        raise NotImplementedError(f"GraphAug.augment should be implemented.")
+
+    def __call__(
+            self, x: torch.FloatTensor,
+            edge_index: torch.LongTensor, edge_weight: Optional[torch.FloatTensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]:
+        return self.augment(Graph(x, edge_index, edge_weight)).unfold()
+
+class Identity(Augmentor):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def augment(self, g: Graph) -> Graph:
+        return g
+
+def drop_node(edge_index: torch.Tensor, edge_weight: Optional[torch.Tensor] = None, keep_prob: float = 0.5):
+    num_nodes = edge_index.max().item() + 1
+    probs = torch.tensor([keep_prob for _ in range(num_nodes)])
+    dist = Bernoulli(probs)
+
+    subset = dist.sample().to(torch.bool).to(edge_index.device)
+    edge_index, edge_weight = subgraph(subset, edge_index, edge_weight)
+
+    return edge_index, edge_weight
+
+
+class Graph(NamedTuple):
+    x: torch.FloatTensor
+    edge_index: torch.LongTensor
+    edge_weights: Optional[torch.FloatTensor]
+
+    def unfold(self) -> Tuple[torch.FloatTensor, torch.LongTensor, Optional[torch.FloatTensor]]:
+        return self.x, self.edge_index, self.edge_weights
+
+
+class Augmentor(ABC):
+    """Base class for graph augmentors."""
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def augment(self, g: Graph) -> Graph:
+        raise NotImplementedError(f"GraphAug.augment should be implemented.")
+
+    def __call__(
+            self, x: torch.FloatTensor,
+            edge_index: torch.LongTensor, edge_weight: Optional[torch.FloatTensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]:
+        return self.augment(Graph(x, edge_index, edge_weight)).unfold()
+
+class NodeDropping(Augmentor):
+    def __init__(self, pn: float):
+        super(NodeDropping, self).__init__()
+        self.pn = pn
+
+    def augment(self, g: Graph) -> Graph:
+        x, edge_index, edge_weights = g.unfold()
+
+        edge_index, edge_weights = drop_node(edge_index, edge_weights, keep_prob=1. - self.pn)
+
+        return Graph(x=x, edge_index=edge_index, edge_weights=edge_weights)
+    
+class EdgeRemoving(Augmentor):
+    def __init__(self, pe: float):
+        super(EdgeRemoving, self).__init__()
+        self.pe = pe
+
+    def augment(self, g: Graph) -> Graph:
+        x, edge_index, edge_weights = g.unfold()
+        edge_index, edge_weights = dropout_adj(edge_index, edge_attr=edge_weights, p=self.pe)
+        return Graph(x=x, edge_index=edge_index, edge_weights=edge_weights)
+    
+class RandomChoice(Augmentor):
+    def __init__(self, augmentors: List[Augmentor], num_choices: int):
+        super(RandomChoice, self).__init__()
+        assert num_choices <= len(augmentors)
+        self.augmentors = augmentors
+        self.num_choices = num_choices
+
+    def augment(self, g: Graph) -> Graph:
+        num_augmentors = len(self.augmentors)
+        perm = torch.randperm(num_augmentors)
+        idx = perm[:self.num_choices]
+        for i in idx:
+            aug = self.augmentors[i]
+            g = aug.augment(g)
+        return g
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, num_features, hidden_channels, num_classes):
+        super(GCN, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = GCNConv(num_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.lin = Linear(hidden_channels, num_classes)
+
+    def forward(self, x, edge_index, batch):
+        # 1. Obtain node embeddings 
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+
+        # 2. Readout layer
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+
+        # 3. Apply a final classifier
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin(x)
+        
+        return x
+    
+def make_gin_conv(input_dim, out_dim):
+    return GINConv(nn.Sequential(nn.Linear(input_dim, out_dim), nn.ReLU(), nn.Linear(out_dim, out_dim)))
+    
+class GConv(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers):
+        super(GConv, self).__init__()
+        self.layers = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+
+        for i in range(num_layers):
+            if i == 0:
+                self.layers.append(make_gin_conv(input_dim, hidden_dim))
+            else:
+                self.layers.append(make_gin_conv(hidden_dim, hidden_dim))
+            self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
+
+        project_dim = hidden_dim * num_layers
+        self.project = torch.nn.Sequential(
+            nn.Linear(project_dim, project_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(project_dim, project_dim))
+
+    def forward(self, x, edge_index, batch):
+        z = x
+        zs = []
+        for conv, bn in zip(self.layers, self.batch_norms):
+            z = conv(z, edge_index)
+            z = F.relu(z)
+            z = bn(z)
+            zs.append(z)
+        gs = [global_add_pool(z, batch) for z in zs]
+        z, g = [torch.cat(x, dim=1) for x in [zs, gs]]
+        return z, g
+    
+class FGNConv(nn.Module):
+    def __init__(self, args, pre_length, input_dim, embed_size, hidden_dim, seq_length, num_layers):
+        super(FGNConv, self).__init__()
+        self.layers = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+
+        for i in range(num_layers):
+            if i == 0:
+                self.layers.append(FGN(args=args, pre_length=pre_length, embed_size=embed_size, \
+                                    feature_size=input_dim, seq_length=seq_length, \
+                                    hidden_size=hidden_dim))
+            else:
+                self.layers.append(FGN(args=args, pre_length=pre_length, embed_size=embed_size, \
+                    feature_size=hidden_dim, seq_length=seq_length, \
+                    hidden_size=hidden_dim))
+            self.batch_norms.append(nn.BatchNorm1d(pre_length))
+
+        project_dim = pre_length * num_layers
+        self.project = torch.nn.Sequential(
+            nn.Linear(project_dim, project_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(project_dim, project_dim))
+
+    def forward(self, x, edge_index, batch):
+        z = x
+        zs = []
+        for conv, bn in zip(self.layers, self.batch_norms):
+            z = conv(z, edge_index)
+            z = F.relu(z)
+            z = bn(z)
+            zs.append(z)
+        gs = [global_add_pool(z, batch) for z in zs]
+        z, g = [torch.cat(x, dim=1) for x in [zs, gs]]
+        return z, g
+    
+class Encoder(torch.nn.Module):
+    def __init__(self, encoder, augmentor):
+        super(Encoder, self).__init__()
+        self.encoder = encoder
+        self.augmentor = augmentor
+
+    def forward(self, x, edge_index, batch):
+        aug1, aug2 = self.augmentor
+        x1, edge_index1, edge_weight1 = aug1(x, edge_index)
+        x2, edge_index2, edge_weight2 = aug2(x, edge_index)
+        z, g = self.encoder(x, edge_index, batch)
+        z1, g1 = self.encoder(x1, edge_index1, batch)
+        z2, g2 = self.encoder(x2, edge_index2, batch)
+        return z, g, z1, z2, g1, g2
+
+class Sampler(ABC):
+    def __init__(self, intraview_negs=False):
+        self.intraview_negs = intraview_negs
+
+    def __call__(self, anchor, sample, *args, **kwargs):
+        ret = self.sample(anchor, sample, *args, **kwargs)
+        if self.intraview_negs:
+            ret = self.add_intraview_negs(*ret)
+        return ret
+
+    @abstractmethod
+    def sample(self, anchor, sample, *args, **kwargs):
+        pass
+
+    @staticmethod
+    def add_intraview_negs(anchor, sample, pos_mask, neg_mask):
+        num_nodes = anchor.size(0)
+        device = anchor.device
+        intraview_pos_mask = torch.zeros_like(pos_mask, device=device)
+        intraview_neg_mask = torch.ones_like(pos_mask, device=device) - torch.eye(num_nodes, device=device)
+        new_sample = torch.cat([sample, anchor], dim=0)                     # (M+N) * K
+        new_pos_mask = torch.cat([pos_mask, intraview_pos_mask], dim=1)     # M * (M+N)
+        new_neg_mask = torch.cat([neg_mask, intraview_neg_mask], dim=1)     # M * (M+N)
+        return anchor, new_sample, new_pos_mask, new_neg_mask
+
+
+class SameScaleSampler(Sampler):
+    def __init__(self, *args, **kwargs):
+        super(SameScaleSampler, self).__init__(*args, **kwargs)
+
+    def sample(self, anchor, sample, *args, **kwargs):
+        assert anchor.size(0) == sample.size(0)
+        num_nodes = anchor.size(0)
+        device = anchor.device
+        pos_mask = torch.eye(num_nodes, dtype=torch.float32, device=device)
+        neg_mask = 1. - pos_mask
+        return anchor, sample, pos_mask, neg_mask
+
+
+class CrossScaleSampler(Sampler):
+    def __init__(self, *args, **kwargs):
+        super(CrossScaleSampler, self).__init__(*args, **kwargs)
+
+    def sample(self, anchor, sample, batch=None, neg_sample=None, use_gpu=True, *args, **kwargs):
+        num_graphs = anchor.shape[0]  # M
+        num_nodes = sample.shape[0]   # N
+        device = sample.device
+
+        if neg_sample is not None:
+            assert num_graphs == 1  # only one graph, explicit negative samples are needed
+            assert sample.shape == neg_sample.shape
+            pos_mask1 = torch.ones((num_graphs, num_nodes), dtype=torch.float32, device=device)
+            pos_mask0 = torch.zeros((num_graphs, num_nodes), dtype=torch.float32, device=device)
+            pos_mask = torch.cat([pos_mask1, pos_mask0], dim=1)     # M * 2N
+            sample = torch.cat([sample, neg_sample], dim=0)         # 2N * K
+        else:
+            assert batch is not None
+            if use_gpu:
+                ones = torch.eye(num_nodes, dtype=torch.float32, device=device)     # N * N
+                pos_mask = scatter(ones, batch, dim=0, reduce='sum')                # M * N
+            else:
+                pos_mask = torch.zeros((num_graphs, num_nodes), dtype=torch.float32).to(device)
+                for node_idx, graph_idx in enumerate(batch):
+                    pos_mask[graph_idx][node_idx] = 1.                              # M * N
+
+        neg_mask = 1. - pos_mask
+        return anchor, sample, pos_mask, neg_mask
+    
+def get_sampler(mode: str, intraview_negs: bool) -> Sampler:
+    if mode in {'L2L', 'G2G'}:
+        return SameScaleSampler(intraview_negs=intraview_negs)
+    elif mode == 'G2L':
+        return CrossScaleSampler(intraview_negs=intraview_negs)
+    else:
+        raise RuntimeError(f'unsupported mode: {mode}')
+    
+    
+def add_extra_mask(pos_mask, neg_mask=None, extra_pos_mask=None, extra_neg_mask=None):
+    if extra_pos_mask is not None:
+        pos_mask = torch.bitwise_or(pos_mask.bool(), extra_pos_mask.bool()).float()
+    if extra_neg_mask is not None:
+        neg_mask = torch.bitwise_and(neg_mask.bool(), extra_neg_mask.bool()).float()
+    else:
+        neg_mask = 1. - pos_mask
+    return pos_mask, neg_mask
+    
+
+class DualBranchContrast(torch.nn.Module):
+    def __init__(self, loss: Loss, mode: str, intraview_negs: bool = False, **kwargs):
+        super(DualBranchContrast, self).__init__()
+        self.loss = loss
+        self.mode = mode
+        self.sampler = get_sampler(mode, intraview_negs=intraview_negs)
+        self.kwargs = kwargs
+
+    def forward(self, h1=None, h2=None, g1=None, g2=None, batch=None, h3=None, h4=None,
+                extra_pos_mask=None, extra_neg_mask=None):
+        if self.mode == 'L2L':
+            assert h1 is not None and h2 is not None
+            anchor1, sample1, pos_mask1, neg_mask1 = self.sampler(anchor=h1, sample=h2)
+            anchor2, sample2, pos_mask2, neg_mask2 = self.sampler(anchor=h2, sample=h1)
+        elif self.mode == 'G2G':
+            assert g1 is not None and g2 is not None
+            anchor1, sample1, pos_mask1, neg_mask1 = self.sampler(anchor=g1, sample=g2)
+            anchor2, sample2, pos_mask2, neg_mask2 = self.sampler(anchor=g2, sample=g1)
+        else:  # global-to-local
+            if batch is None or batch.max().item() + 1 <= 1:  # single graph
+                assert all(v is not None for v in [h1, h2, g1, g2, h3, h4])
+                anchor1, sample1, pos_mask1, neg_mask1 = self.sampler(anchor=g1, sample=h2, neg_sample=h4)
+                anchor2, sample2, pos_mask2, neg_mask2 = self.sampler(anchor=g2, sample=h1, neg_sample=h3)
+            else:  # multiple graphs
+                assert all(v is not None for v in [h1, h2, g1, g2, batch])
+                anchor1, sample1, pos_mask1, neg_mask1 = self.sampler(anchor=g1, sample=h2, batch=batch)
+                anchor2, sample2, pos_mask2, neg_mask2 = self.sampler(anchor=g2, sample=h1, batch=batch)
+
+        pos_mask1, neg_mask1 = add_extra_mask(pos_mask1, neg_mask1, extra_pos_mask, extra_neg_mask)
+        pos_mask2, neg_mask2 = add_extra_mask(pos_mask2, neg_mask2, extra_pos_mask, extra_neg_mask)
+        l1 = self.loss(anchor=anchor1, sample=sample1, pos_mask=pos_mask1, neg_mask=neg_mask1, **self.kwargs)
+        l2 = self.loss(anchor=anchor2, sample=sample2, pos_mask=pos_mask2, neg_mask=neg_mask2, **self.kwargs)
+
+        return (l1 + l2) * 0.5
